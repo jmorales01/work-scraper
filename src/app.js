ee79dd047b364caf842ae62f4ff2fba9e2cea41f
jmorales01 <joseluismoralesvega24@gmail.com>
2024-07-21 22:53:25 -0500

    feat: modificando api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import holamundo from './modules/holamundo/routes.js';
+import helloworld from './modules/helloworld/routes.js';
 import cors from 'cors';
 
 const app = express();
@@ -10,6 +10,6 @@ app.use(cors());
 app.set('port', process.env.PORT || 3000);
 
 // Rutas
-app.use('/api/holamundo', holamundo);
+app.use('/api/helloworld', helloworld);
 
 export { app };
